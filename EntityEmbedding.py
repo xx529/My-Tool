271527226c5de9d9de46e4e665f76abde26cb6eb
Hang <64747715+xx529@users.noreply.github.com>
2021-03-29 23:25:00 +0800

    Update EntityEmbedding.py

--- EntityEmbedding.py
+++ EntityEmbedding.py
@@ -1,1 +1,27 @@
-1
+import torch.nn as nn
+import torch
+
+class EntityEmbedding(nn.Module):
+    def __init__(self, bins_center, embedding_dim, eps=1e-8):
+        super(EntityEmbedding, self).__init__()
+        self.embedding = nn.Embedding(len(bins_center), embedding_dim)
+        self.softmax = nn.Softmax(dim=1)
+        self.bins_center = torch.FloatTensor(bins_center).detach_()
+        self.eps = eps
+        
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        w = self.softmax((1 / ((x - self.bins_center).abs() + self.eps)))
+        v = torch.mm(w, self.embedding.weight)
+        return v
+        
+        
+# -------------------------        
+bins_center = [1,2,3,4,5] # class List
+emb_dim = 3 
+x = torch.FloatTensor([1,2,3,4,5]) # dim=(n,)
+
+entityembedding = EntityEmbedding(bins_center, emb_dim)
+emb_vector = entityembedding(x)
+
+# result the same as entityembedding.embedding.weigth
