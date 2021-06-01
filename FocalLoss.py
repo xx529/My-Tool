832f623373b433b5e60b9ad5071b2569ff1e3214
Hang <64747715+xx529@users.noreply.github.com>
2021-06-01 16:04:31 +0800

    Update FocalLoss.py

--- FocalLoss.py
+++ FocalLoss.py
@@ -1,1 +1,12 @@
-# 二分类 focalloss
+# 二分类 FocalLoss
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, y_pre, y_true):
+        loss_l = y_true * torch.pow(1 - y_pre, self.gamma) * torch.log1p(y_pre)
+        loss_r = (1 - y_true) * torch.pow(y_pre, self.gamma) * torch.log1p(1 - y_pre)
+        loss = torch.mean(loss_l + loss_r)
+        return loss
